d3d05e2f40a6142a6770b8c081c42f8da3692d7a
icaroquadra <icaroquadra@gmail.com>
2020-02-06 19:57:15 -0300

    created models product productionline e reports

---
+++ src/production_line/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProductionLineConfig(AppConfig):
+    name = 'production_line'
---
+++ src/reports/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from products.models import Product
+from production_line.models import ProductionLine
+
+# Create your models here.
+
+# hours = (
+#     ("1", "1"),
+#     ("2", "2"),
+#     ("3", "3"),
+#     ...
+# )
+
+hours = ([(str(x), str(x)) for x in range (1,25)])    
+
+class Report(models.Model):
+    day = models.DateField(default=timezone.now)
+    start_hour = models.CharField(max_length=2, choices=hours)
+    end_hour = models.CharField(max_length=2, choices=hours)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    plan = models.PositiveIntegerField()
+    execution = models.PositiveIntegerField()
+    procution_line = models.ForeignKey(ProductionLine, on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.start_hour, self.end_hour, self.procution_line)
+    
